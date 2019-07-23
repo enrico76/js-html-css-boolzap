98322e03f14f0a2453ce042c2a357922c77bced6
enrico76 <enricodip@gmail.com>
2019-07-23 17:24:21 +0200

    initial function javascript

--- myscript.js
+++ myscript.js
@@ -1,1 +1,14 @@
 console.log("initial myscript");
+
+$( document ).ready(function(){
+$("#iconSend").click(invioMsg);
+
+
+
+
+// funzione di invio messaggio
+function invioMsg () {
+  var invioUtente = $("#myText").val();
+  console.log(invioUtente);
+}
+});
