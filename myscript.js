aae1088681f57616e08fcdb70e90fdff4594bb0d
enrico76 <enricodip@gmail.com>
2019-07-26 08:50:12 +0200

    add swap name chat and avatar

--- myscript.js
+++ myscript.js
@@ -46,12 +46,18 @@ function invioMsg () {
     var contactsel = $(this).find("b").text();
     $(".section_up .namechat").text(contactsel);
     var imgcontact = $(this).find("img").attr("src");
-    $(".section_up imgcontact").not("#batman").attr("src" , imgcontact);
+    $(".section_up img").not("#batman").attr("src" , imgcontact);
     var chat = $(this).attr("chatnumber");
     console.log(chat);
-  })
-  // var chatCont = {};
 
+    $(".msg_container").removeClass("active");
+    $('.msg_container[chat = "'+ chat +'"]').addClass("active");
+  })
+
+    // $(".msg_container").on("click" , ".verdi" , function() {
+    //   $(this).children
+    //
+    // })
 
 
 });
